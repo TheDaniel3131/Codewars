2be71ef198135ccc3cf6b144a423058c783f562a
zephrous <danielpoh3131@gmail.com>
2024-03-04 14:16:55 +0800

    add

--- 5kyu/ROT13/solve.py
+++ 5kyu/ROT13/solve.py
@@ -2,3 +2,11 @@ from codecs import encode
 
 def rot13(message):
     return encode(message, "rot13")
+    
+    
+ 
+# No cheating solution   
+# I didn't see the last sentence lol
+
+def rot13(message):
+    return message.translate(str.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"))
