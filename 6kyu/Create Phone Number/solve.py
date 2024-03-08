90c8a65a2fb8813a455d4e8edaa2ff7b55fc891d
zephrous <danielpoh3131@gmail.com>
2024-03-08 15:43:07 +0800

    update

--- 6kyu/Create Phone Number/solve.py
+++ 6kyu/Create Phone Number/solve.py
@@ -1,2 +1,18 @@
 def create_phone_number(n):
     return f"({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}"
+    
+def create_phone_number(n):
+	return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
+    
+    
+def create_phone_number(n):
+    m = ''.join(map(str, n))
+    return f"({m[:3]}) {m[3:6]}-{m[6:]}"
+    
+def create_phone_number(n):
+  str1 =  ''.join(str(x) for x in n[0:3])
+  str2 =  ''.join(str(x) for x in n[3:6])
+  str3 =  ''.join(str(x) for x in n[6:10])
+
+
+  return '({}) {}-{}'.format(str1, str2, str3)
