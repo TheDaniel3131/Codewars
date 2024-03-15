3e2d6e289ad0bbe8c8a3ad5de13712cd72b19950
zephrous <danielpoh3131@gmail.com>
2024-03-15 16:16:05 +0800

    update

--- 8kyu/Abbreviate a Two Word Name/solve.py
+++ 8kyu/Abbreviate a Two Word Name/solve.py
@@ -6,3 +6,15 @@ def abbrev_name(name):
     # print(last_name)
 
     return (first_name[0] + "." + last_name[0]).upper()
+    
+def abbrevName(name):
+    return '.'.join(w[0] for w in name.split()).upper()
+    
+ 
+def abbrevName(name):
+    first, last = name.upper().split(' ')
+    return first[0] + '.' + last[0]
+    
+def abbrevName(name):
+    names = name.split()
+    return f"{names[0][0]}.{names[1][0]}".upper()
