37756d58e00b1ce02e1ddf3ed7c02bdaa02ac507
zephrous <danielpoh3131@gmail.com>
2024-03-17 21:39:48 +0800

    update

--- 8kyu/A Needle in the Haystack/solve.py
+++ 8kyu/A Needle in the Haystack/solve.py
@@ -2,3 +2,7 @@ def find_needle(haystack):
     sentence = "found the needle at position "
     if "needle" in haystack:
         return sentence + str(haystack.index("needle"))
+    
+    
+def find_needle(haystack):
+    return f'found the needle at position {haystack.index("needle")}'
