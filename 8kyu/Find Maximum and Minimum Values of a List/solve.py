f22b1af9ed5207190fb3a0e0f93333dc8204bc1f
zephrous <danielpoh3131@gmail.com>
2024-03-17 22:18:52 +0800

    update

---
+++ 8kyu/Find Maximum and Minimum Values of a List/solve.py
@@ -0,0 +1,28 @@
+def minimum(arr): 
+    return min(arr) if arr else 0 
+    
+
+def maximum(arr): 
+    return max(arr) if arr else 0
+    
+
+
+def minimum(arr): 
+    return min(arr) 
+    
+
+def maximum(arr): 
+    return max(arr) 
+    
+    
+def minimum(arr):
+    return sorted(array[0])
+    
+
+def minimum(arr):
+    # print(sorted(arr)[0])
+    return sorted(arr)[0]
+
+def maximum(arr):
+    # print(sorted(arr)[-1])
+    return sorted(arr)[-1]
