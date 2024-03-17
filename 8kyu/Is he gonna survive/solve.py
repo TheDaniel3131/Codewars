8fa9bcef33fcd4477ca538608cd0e736391446f3
zephrous <danielpoh3131@gmail.com>
2024-03-17 21:56:37 +0800

    update

--- 8kyu/Is he gonna survive/solve.py
+++ 8kyu/Is he gonna survive/solve.py
@@ -2,3 +2,15 @@ def hero(bullets, dragons):
     if bullets == 0 or bullets < dragons:
         return False
     return (bullets // dragons) % 2 == 0
+
+
+def hero(bullets, dragons):
+    return bullets >= dragons * 2
+
+
+
+def hero(bullets, dragons):    
+    return (1 if bullets//dragons == 2 else 0)
+
+def hero(bullets, dragons):
+    return int(bullets) >= int(dragons * 2)
