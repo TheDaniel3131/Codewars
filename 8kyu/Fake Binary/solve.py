432c1236b95a59f3b57bfa6d1e9020656cce54e4
zephrous <danielpoh3131@gmail.com>
2024-03-17 22:25:33 +0800

    update

--- 8kyu/Fake Binary/solve.py
+++ 8kyu/Fake Binary/solve.py
@@ -7,3 +7,9 @@ def fake_bin(x):
             new_x += "1"
     return new_x
     
+def fake_bin(x):
+    map = str.maketrans('123456789', '000011111')
+    return x.translate(map)
+
+def fake_bin(x):
+    return ''.join('0' if c < '5' else '1' for c in x)
