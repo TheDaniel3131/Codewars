b8d3fbce5a49760bb9f31bffb48232ba014c4ee7
zephrous <danielpoh3131@gmail.com>
2024-03-19 21:48:57 +0800

    update

--- 8kyu/Thinkful - Logic Drills Traffic light/solve.py
+++ 8kyu/Thinkful - Logic Drills Traffic light/solve.py
@@ -5,3 +5,20 @@ def update_light(current):
         return "red"
     else:
         return "yellow"
+    
+def update_light(current):
+    return {"green": "yellow", "yellow": "red", "red": "green"}[current]
+
+
+def update_light(current):
+    light_order = {'red':'green', 'yellow':'red', 'green':'yellow'}
+    
+    return light_order[current]
+
+def update_light(current):
+    match current:
+        case 'red': return 'green'
+        case 'yellow': return 'red'
+        case 'green': return 'yellow'
+        
+update_light = {"green":"yellow", "yellow":"red", "red":"green"}.get
