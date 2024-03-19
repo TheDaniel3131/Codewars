62078e76103bca20c6ae74390b6f6cb4930cb6f8
zephrous <danielpoh3131@gmail.com>
2024-03-19 16:08:04 +0800

    update

--- 8kyu/Sleigh Authentication/solve.py
+++ 8kyu/Sleigh Authentication/solve.py
@@ -1,3 +1,11 @@
 class Sleigh(object):
     def authenticate(self, name, password):
         return name == 'Santa Claus' and password == 'Ho Ho Ho!'
+        
+ 
+
+class Sleigh(object):
+    def authenticate(self, x, y):
+        if (x,y) == ('Santa Claus','Ho Ho Ho!'):
+            return True
+        return False
