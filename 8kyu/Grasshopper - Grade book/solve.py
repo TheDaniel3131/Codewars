378addf800ad3ba864aa6d17a42817c6a764a3d1
zephrous <danielpoh3131@gmail.com>
2024-03-19 10:56:56 +0800

    update

--- 8kyu/Grasshopper - Grade book/solve.py
+++ 8kyu/Grasshopper - Grade book/solve.py
@@ -9,3 +9,6 @@ def get_grade(s1, s2, s3):
     elif 60 <= f < 70:
         return 'D'
     return 'F'
+
+def get_grade(s1, s2, s3):
+    return {6:'D', 7:'C', 8:'B', 9:'A', 10:'A'}.get((s1 + s2 + s3) / 30, 'F')
