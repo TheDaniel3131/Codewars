cb6c531b3009193890c555293dd0f0cd492e80e5
zephrous <danielpoh3131@gmail.com>
2024-03-19 22:07:18 +0800

    update

--- 8kyu/Reversed Words/solve.py
+++ 8kyu/Reversed Words/solve.py
@@ -1,3 +1,15 @@
 def reverse_words(s):
     s = s.split(' ')
     return ' '.join(reversed(s))
+
+
+def reverseWords(s):
+    return " ".join(s.split(" ")[::-1])
+
+
+def reverseWords(s):
+    return ' '.join(reversed(s.split(' ')))
+
+
+def reverseWords(s):
+    return ' '.join(reversed(s.split()))
