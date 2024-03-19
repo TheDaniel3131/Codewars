ecd881a7ce26c32fe058942438aad8793ec7a27b
zephrous <danielpoh3131@gmail.com>
2024-03-19 15:08:34 +0800

    update

---
+++ 8kyu/L1 Bartender, drinks!/solve.py
@@ -0,0 +1,10 @@
+def get_drink_by_profession(person):
+    drinks = {
+        "jabroni": "Patron Tequila",
+        "school counselor": "Anything with Alcohol",
+        "programmer": "Hipster Craft Beer",
+        "bike gang member": "Moonshine",
+        "politician": "Your tax dollars",
+        "rapper": "Cristal"
+    }
+    return drinks.get(person.lower(), "Beer")
