bb6a65708a2f184ec62d0eb27b5cedfcaa28234c
zephrous <danielpoh3131@gmail.com>
2024-03-20 09:33:36 +0800

    update

--- 8kyu/Beginner Series #4 Cockroach/solve.py
+++ 8kyu/Beginner Series #4 Cockroach/solve.py
@@ -2,3 +2,15 @@ def cockroach_speed(s):
     if s <= 0:
         return 0
     return int(s * 27.778)
+    
+def cockroach_speed(s):
+    return s // 0.036
+    
+import math
+def cockroach_speed(s):
+    return math.floor(s * 27.778)
+    
+def cockroach_speed(s):
+    cm_per_km = 100000
+    sec_per_hour = 3600
+    return int(s * cm_per_km / sec_per_hour)
