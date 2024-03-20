c8e84422718c2038302085c760817b48a212a2f3
zephrous <danielpoh3131@gmail.com>
2024-03-20 09:57:55 +0800

    update

--- 8kyu/Get Planet Name By ID/solve.py
+++ 8kyu/Get Planet Name By ID/solve.py
@@ -11,3 +11,31 @@ def get_planet_name(id):
         case 7: name = "Uranus"  
         case 8: name = "Neptune"
     return name
+
+
+def get_planet_name(id):
+    return {
+        1: "Mercury",
+        2: "Venus",
+        3: "Earth",
+        4: "Mars",
+        5: "Jupiter",
+        6: "Saturn",
+        7: "Uranus",
+        8: "Neptune",
+    }.get(id, None)
+
+
+
+def get_planet_name(id):
+    planets = {
+        1: "Mercury",
+        2: "Venus",
+        3: "Earth",
+        4: "Mars",
+        5: "Jupiter",
+        6: "Saturn",
+        7: "Uranus", 
+        8: "Neptune",
+    }
+    return planets[id]
