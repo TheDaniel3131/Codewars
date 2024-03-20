449ba10829c59f233c56885c883d96be91f77cd4
zephrous <danielpoh3131@gmail.com>
2024-03-20 10:53:23 +0800

    update

--- 4kyu/Strip Comments/solve.py
+++ 4kyu/Strip Comments/solve.py
@@ -9,3 +9,9 @@ import re
 
 def strip_comments(text, markers):
     return re.sub('|'.join('( |\t)*\\' + m + '[^\n]*' for m in markers), '', text)
+    
+    
+def solution(text, markers):
+	for marker in markers:
+		text = text.replace(marker, 'YOUCANTCATCHME')
+	return '\n'.join(sentence.split('YOUCANTCATCHME')[0].rstrip() for sentence in text.splitlines())
