df08f394c2da5dce78e768ff0a7445a5e75e0832
zephrous <danielpoh3131@gmail.com>
2024-03-20 09:56:46 +0800

    update

---
+++ 8kyu/Get Planet Name By ID/solve.py
@@ -0,0 +1,13 @@
+def get_planet_name(id):
+    # It is fixed.
+    name=""
+    match id:
+        case 1: name = "Mercury"
+        case 2: name = "Venus"
+        case 3: name = "Earth"
+        case 4: name = "Mars"
+        case 5: name = "Jupiter"
+        case 6: name = "Saturn"
+        case 7: name = "Uranus"  
+        case 8: name = "Neptune"
+    return name
