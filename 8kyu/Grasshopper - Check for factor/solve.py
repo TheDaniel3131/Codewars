29b75da24ac129209fc8def3766b630bbdd64612
zephrous <danielpoh3131@gmail.com>
2024-03-20 09:22:27 +0800

    update

---
+++ 8kyu/Grasshopper - Check for factor/solve.py
@@ -0,0 +1,9 @@
+def check_for_factor(base, factor):
+    return base % factor == 0
+    
+    
+def check_for_factor(base, factor):
+    return not (base % factor)
+
+
+check_for_factor = lambda _,__: not _%__
