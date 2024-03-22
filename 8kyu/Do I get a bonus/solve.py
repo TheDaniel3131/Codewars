11611d3658ccebe338330edaa66a31141248f1e2
zephrous <danielpoh3131@gmail.com>
2024-03-22 21:40:46 +0800

    update

--- 8kyu/Do I get a bonus/solve.py
+++ 8kyu/Do I get a bonus/solve.py
@@ -1,2 +1,8 @@
 def bonus_time(salary, bonus):
     return f"${salary * 10}" if bonus == 1 else f"${salary}"
+
+def bonus_time(salary, bonus):
+    return "${}".format(salary * (10 if bonus else 1)
+                        
+def bonus_time(salary, bonus):
+    return '$' + str(salary * [1,10][bonus])
