c0e420ce21eac534f29c6d5fd60b2104a81955d4
zephrous <danielpoh3131@gmail.com>
2024-03-22 22:08:41 +0800

    up

--- 8kyu/Expressions Matter/solve.py
+++ 8kyu/Expressions Matter/solve.py
@@ -1,3 +1,12 @@
 def expression_matter(a, b, c):
     # output possible expressions with maximum value
     return max(a * (b + c), a * b * c, (a + b) * c, a + b + c)
+
+
+def expression_matter(a, b, c):
+    operations = '+*'
+    return max(
+        max(eval(f'({a}{op1}{b}){op2}{c}'), eval(f'{a}{op1}({b}{op2}{c})'))
+        for op1 in operations
+        for op2 in operations
+    )
