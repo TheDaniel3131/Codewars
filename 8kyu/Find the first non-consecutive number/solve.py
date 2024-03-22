c7524a0fc34e1da2a9ff317c051915917c35164a
zephrous <danielpoh3131@gmail.com>
2024-03-22 21:21:10 +0800

    update

--- 8kyu/Find the first non-consecutive number/solve.py
+++ 8kyu/Find the first non-consecutive number/solve.py
@@ -10,3 +10,30 @@ def first_non_consecutive(arr):
     for i, x in enumerate(arr[:-1]):
         if x + 1 != arr[i + 1]:
             return arr[i + 1]
+        
+def first_non_consecutive(arr):
+    for i in range(len(arr)-1):
+        if arr[i]+1!=arr[i+1]:
+            return arr[i+1]
+    return None
+
+def first_non_consecutive(arr):
+    num = arr[0] 
+    
+    for i in range(len(arr)):
+        if num == arr[i]:
+            num += 1
+        else:
+            return arr[i]
+        
+def first_non_consecutive(arr):
+    return next((j for i, j in zip(arr, arr[1:]) if i + 1 != j), None)
+
+
+def first_non_consecutive(a):
+    i = a[0]
+    for e in a:
+        if e != i:
+            return e
+        i += 1
+    return None
