b28c18fab80c1defb8c2f94213f566645ff910b2
zephrous <danielpoh3131@gmail.com>
2024-03-22 21:34:35 +0800

    update

---
+++ 8kyu/Is this even/solve.py
@@ -0,0 +1,7 @@
+def is_even(n) 
+    return n % 2 == 0
+
+def is_even(n):
+    return not n % 2
+
+is_even = lambda n: n % 2 == 0
---
+++ 8kyu/altERnaTIng cAsE = ALTerNAtiNG CaSe/solve.py
@@ -0,0 +1,4 @@
+def to_alternating_case(s):
+    return s.swapcase()
+
+
