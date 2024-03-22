db5e4bd13fa53e627d4bd278bccf8327348b100e
zephrous <danielpoh3131@gmail.com>
2024-03-22 22:00:06 +0800

    update

--- 8kyu/Student's Final Grade/solve.py
+++ 8kyu/Student's Final Grade/solve.py
@@ -7,3 +7,13 @@ def final_grade(exam, projects):
         return 75
     else:
         return 0
+    
+    
+def final_grade(exam, projects):
+    if exam > 90 or  projects > 10: return 100
+    if exam > 75 and projects >= 5: return 90
+    if exam > 50 and projects >= 2: return 75
+    return 0
+
+def final_grade(exam, projects):
+    return 100 if exam > 90 or projects > 10 else 90 if exam > 75 and projects >= 5 else 75 if exam > 50 and projects >=2 else 0
