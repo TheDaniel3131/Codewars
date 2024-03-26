d84c87cabf3352d97cdca5f42df3d9951307a7ff
zephrous <danielpoh3131@gmail.com>
2024-03-26 21:24:54 +0800

    update

--- 8kyu/Thinkful - Dictionary drills Order filler/solve.py
+++ 8kyu/Thinkful - Dictionary drills Order filler/solve.py
@@ -3,3 +3,14 @@ def fillable(stock, merch, n):
         return True
     else:
         return False
+
+def fillable(stock, merch, n):
+    return merch in stock and stock[merch] >= n
+
+
+def fillable(stock, merch, n):
+    return stock.get(merch, 0) >= n
+
+
+def fillable(stock, merch, n):
+    return stock.get(merch, False) >= n  
