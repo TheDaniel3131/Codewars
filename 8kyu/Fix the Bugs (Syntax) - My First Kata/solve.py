9e132d4bb4ec911717c4b09b29535c47f5792876
zephrous <danielpoh3131@gmail.com>
2024-03-26 21:38:51 +0800

    update

--- 8kyu/Fix the Bugs (Syntax) - My First Kata/solve.py
+++ 8kyu/Fix the Bugs (Syntax) - My First Kata/solve.py
@@ -3,3 +3,23 @@ def my_first_kata(a,b):
         return a % b ++ b % a
     else:
         return False
+    
+    
+    
+def my_first_kata(a,b):
+    #your code here
+    if type(a) == int and type(b) == int:
+        return a % b + b % a
+    else:
+        return False
+    
+    
+def my_first_kata(a, b):
+    try:
+        return a % b + b % a
+    except (TypeError, ZeroDivisionError):
+        return False
+    
+    
+def my_first_kata(a,b):
+    return type(a) == type(b) == int and a % b + b % a
