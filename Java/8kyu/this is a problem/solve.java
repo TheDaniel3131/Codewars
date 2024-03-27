b7e681d1d58030afc7d85ab27367104a7e726529
zephrous <danielpoh3131@gmail.com>
2024-03-27 15:29:44 +0800

    update

--- Java/8kyu/this is a problem/solve.java
+++ Java/8kyu/this is a problem/solve.java
@@ -21,3 +21,41 @@ public class NameMe {
         return this.fullName;
     }
 }
+
+
+
+public class NameMe {
+    public String firstName;
+    public String lastName;
+    public String fullName;
+
+    public NameMe(String first, String last) {
+        this.setFirstName(first);
+        this.setLastName(last);
+        this.setFullName(first+" " +last);
+   }
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+ }
