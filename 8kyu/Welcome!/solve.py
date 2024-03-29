3243e97d934b70ae7fa325c61cccf62056833408
zephrous <danielpoh3131@gmail.com>
2024-03-29 10:30:40 +0800

    upadte

--- 8kyu/Welcome!/solve.py
+++ 8kyu/Welcome!/solve.py
@@ -46,3 +46,29 @@ def greet(language):
         'swedish': 'Valkommen',
         'welsh': 'Croeso'
     }.get(language, 'Welcome')
+
+
+def greet(language):
+    #your code here
+    dict = {'english': 'Welcome',
+            'czech': 'Vitejte',
+            'danish': 'Velkomst',
+            'dutch': 'Welkom',
+            'estonian': 'Tere tulemast',
+            'finnish': 'Tervetuloa',
+            'flemish': 'Welgekomen',
+            'french': 'Bienvenue',
+            'german': 'Willkommen',
+            'irish': 'Failte',
+            'italian': 'Benvenuto',
+            'latvian': 'Gaidits',
+            'lithuanian': 'Laukiamas',
+            'polish': 'Witamy',
+            'spanish': 'Bienvenido',
+            'swedish': 'Valkommen',
+            'welsh': 'Croeso'}
+
+    if language in dict.keys():
+        return (dict[language])
+    else:
+        return ('Welcome')
