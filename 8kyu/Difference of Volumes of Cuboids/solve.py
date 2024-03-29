0aabbda36ea30f71e706efb932cbcd946068e975
zephrous <danielpoh3131@gmail.com>
2024-03-29 10:18:52 +0800

    update

--- 8kyu/Difference of Volumes of Cuboids/solve.py
+++ 8kyu/Difference of Volumes of Cuboids/solve.py
@@ -8,3 +8,18 @@ def find_difference(a, b):
     return abs(prod(a) - prod(b))
 
 
+def find_difference(a, b):
+    A = B = 1
+    for i, j in zip(a, b):
+        A *= i
+        B *= j
+    return abs(A - B)
+
+
+from operator import mul
+from functools import reduce
+def find_difference(a, b):
+    return abs(reduce(mul, a) - reduce(mul, b))
+
+
+find_difference = lambda (a, b, c), (x, y, z): abs(a * b * c - x * y * z)
