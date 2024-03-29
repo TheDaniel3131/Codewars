8fe61b8a224fbe0865c0166e15437ce1f6e5a1b0
zephrous <danielpoh3131@gmail.com>
2024-03-29 10:22:03 +0800

    update

--- 8kyu/Reverse List Order/solve.py
+++ 8kyu/Reverse List Order/solve.py
@@ -1,3 +1,19 @@
 def reverse_list(l):
     return l[::-1]
 
+def reverse_list(l):
+    return list(reversed(l))
+
+    
+def reverse_list(l):
+  new_list = []
+  for i in l:
+      new_list = [i] + new_list
+  return new_list
+
+reverse_list = lambda l: l.reverse() or l
+
+
+def reverse_list(l):
+    l.reverse()
+    return l
