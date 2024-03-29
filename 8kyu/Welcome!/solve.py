08f14958e36323f84ca9213534bde0bc282d6001
zephrous <danielpoh3131@gmail.com>
2024-03-29 10:29:29 +0800

    update

---
+++ 8kyu/Welcome!/solve.py
@@ -0,0 +1,25 @@
+def greet(language):
+    welcome_dict = {
+        "english": "Welcome",
+        "czech": "Vitejte",
+        "danish": "Velkomst",
+        "dutch": "Welkom",
+        "estonian": "Tere tulemast",
+        "finnish": "Tervetuloa",
+        "flemish": "Welgekomen",
+        "french": "Bienvenue",
+        "german": "Willkommen",
+        "irish": "Failte",
+        "italian": "Benvenuto",
+        "latvian": "Gaidits",
+        "lithuanian": "Laukiamas",
+        "polish": "Witamy",
+        "spanish": "Bienvenido",
+        "swedish": "Valkommen",
+        "welsh": "Croeso"
+    }
+    
+    # dictionary.get(keyname, value)
+    # keyname - Required. The keyname of the item you want to return the value from.
+    # value	- Optional. A value to return if the specified key does not exist. Default value None.
+    return welcome_dict.get(language, 'Welcome')
