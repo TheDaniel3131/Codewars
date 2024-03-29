8316ea6c3bdb93598f503a6509298858bed661ab
zephrous <danielpoh3131@gmail.com>
2024-03-29 21:26:27 +0800

    update

---
+++ 8kyu/Unfinished Loop - Bug Fixing #1/solve.py
@@ -0,0 +1,24 @@
+def create_array(n):
+    res = []
+    i = 1
+    for i in range(i, n+1):
+        res += [i]
+    return res
+
+
+def create_array(n):
+    res=[]
+    i=1
+    while i<=n:
+        res+=[i]
+        i+= 1
+    return res
+
+
+def create_array(n):
+    return [i for i in range(1,n+1)]
+
+
+
+def create_array(n):
+    return list(range(1,n + 1))
