fe54fca2b3f34b385c1ff744eaa498b4dafb3155
zephrous <danielpoh3131@gmail.com>
2024-03-29 23:01:33 +0800

    update

---
+++ 8kyu/Quadrants/solve.py
@@ -0,0 +1,13 @@
+def quadrant(x, y):
+    if x > 0 and y >= 0:
+        return 1
+    elif x > 0 and y < 0:
+        return 4;
+    elif x < 0 and y >= 0:
+        return 2;
+    elif x < 0 and y << 0:
+        return 3;
+    else:
+        return -1;
+    
+    
