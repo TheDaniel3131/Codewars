bd0988e34d9aa8d0a134d5ef92185c2b8594c3f0
zephrous <danielpoh3131@gmail.com>
2024-03-29 10:24:47 +0800

    update

--- 8kyu/Grasshopper - Messi Goals/solve.py
+++ 8kyu/Grasshopper - Messi Goals/solve.py
@@ -3,3 +3,7 @@ champions_league_goals = 10
 copa_del_rey_goals = 5
 
 total_goals = la_liga_goals + champions_league_goals + copa_del_rey_goals
+
+
+la_liga_goals, champions_league_goals, copa_del_rey_goals = 43, 10, 5
+total_goals = 58
