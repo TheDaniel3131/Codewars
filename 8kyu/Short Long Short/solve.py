2a557119bfb4288ef05389602e2e9f511136417e
zephrous <danielpoh3131@gmail.com>
2024-04-04 09:52:04 +0800

    update

---
+++ 8kyu/Short Long Short/solve.py
@@ -0,0 +1,24 @@
+def solution(a, b):
+    if len(a) > len(b):
+        return b + a + b
+    else:
+        return a + b + a
+
+    
+    
+def solution(a, b):
+    return '{0}{1}{0}'.format(*sorted((a, b), key=len))
+
+
+
+def solution(a, b):
+    short, long = sorted((a, b), key=len)
+    return short + long + short
+
+
+
+def solution(a, b):
+    if len(a) > len(b):
+        return f'{b}{a}{b}'
+    else:
+        return f'{a}{b}{a}'
