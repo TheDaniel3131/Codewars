4d3c4f28dd198e16b020f1a2a9c0baf1f1e5e46a
zephrous <danielpoh3131@gmail.com>
2024-04-10 17:49:51 +0800

    update

--- 8kyu/A Strange Trip to the Market/solve.py
+++ 8kyu/A Strange Trip to the Market/solve.py
@@ -8,3 +8,13 @@ def is_loch_ness_monster(string):
             if i in string:
                 return True;
         return False;
+    
+
+def is_lock_ness_monster(s):
+    return any(i in s for i in ('tree fiddy', 'three fifty', '3.50'))
+
+
+import re
+
+def is_lock_ness_monster(s):
+    return bool(re.search(r"3\.50|tree fiddy|three fifty", s))
