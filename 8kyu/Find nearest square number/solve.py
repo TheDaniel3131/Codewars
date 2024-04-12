2259939b531c5c7557f97e0e14c2ea92462dda30
zephrous <danielpoh3131@gmail.com>
2024-04-12 18:25:41 +0800

    update

---
+++ 8kyu/Find nearest square number/solve.py
@@ -0,0 +1,12 @@
+def nearest_sq(n):
+    return round(n ** 0.5) ** 2
+
+
+def nearest_sq(n):
+    return round(n**.5)**2
+
+
+from math import sqrt
+
+def nearest_sq(n):
+    return pow(round(sqrt(n)), 2)
