10032b247fd7eeff83cc0bcd93b72c2312071088
zephrous <danielpoh3131@gmail.com>
2024-04-15 15:46:55 +0800

    update

---
+++ 8kyu/Lario and Muigi Pipe Problem/solve.py
@@ -0,0 +1,10 @@
+def pipe_fix(nums):
+    return list(range(nums[0], nums[-1] + 1))
+
+
+def pipe_fix(nums):
+    return list(range(min(nums), max(nums) + 1))
+
+
+def pipe_fix(l):
+	return [x for x in range(min(l), max(l)+1)]
